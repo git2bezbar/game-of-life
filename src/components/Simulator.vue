d619e45620898285c5ac1f214e8af01fec8f1488
LeonardPaillet <66920564+LeonardPaillet@users.noreply.github.com>
2024-05-17 17:34:32 +0200

    feature born cells

--- src/components/Simulator.vue
+++ src/components/Simulator.vue
@@ -1,65 +1,134 @@
-
 <script setup>
-import SimulatorCell from "./SimulatorCell.vue"
+import SimulatorCell from './SimulatorCell.vue'
 </script>
 <template>
   <div class="grid" :style="{ 'grid-template-columns': 'repeat(' + x + ', 50px)' }">
     <div v-for="(row, rowIndex) in grid" :key="rowIndex">
-      <SimulatorCell 
+      <SimulatorCell
         v-bind:row="row"
         v-bind:rowIndex="rowIndex"
         v-for="(cell, cellIndex) in row"
         :key="cellIndex"
         v-bind:cell="cell"
         v-bind:cellIndex="cellIndex"
-        :initialIsAlive="false" 
-        @update:isAlive="updateCell(index, $event)"
+        :isAliveParent="getCell(cellIndex, rowIndex)"
+        @update:isAlive="updateCell(cellIndex, rowIndex, $event)"
       />
     </div>
-    
   </div>
   <button @click="nextGeneration">Génération Suivante</button>
 </template>
 
 <script>
 export default {
-  components:{
+  components: {
     SimulatorCell
   },
-	data() {
-		return {
-      x:10,
-			y:10,
-      grid: []
-		}
-	},
-  computed:{
-    count(){
-      return this.x*this.y
+  data() {
+    return {
+      x: 10,
+      y: 10,
+      grid: [],
+      nextGrid: []
     }
   },
-  methods:{
-    nextGeneration(){
-      const SimulatorCellComponent = this.$options.components.SimulatorCell;
-      console.table(this.grid);
+  computed: {
+    count() {
+      return this.x * this.y
+    }
+  },
+  methods: {
+    nextGeneration() {
+      this.nextGrid = JSON.parse(JSON.stringify(this.grid))
+      for (let i = 0; i < this.grid.length; i++) {
+        for (let j = 0; j < this.grid[i].length; j++) {
+          let cellsAroundAlive = 0
+          //console.log(this.grid[i][j])
+          if (this.grid[i][j] === false) {
+            this.nextGrid[i][j] = this.willBeBorn(i, j, cellsAroundAlive)
+          }
+        }
+      }
+      console.log('Grid normal : ')
+      console.table(this.grid)
+      console.log('Prochaine grid : ')
+      console.table(this.nextGrid)
+      this.grid = JSON.parse(JSON.stringify(this.nextGrid))
     },
-    updateCell(index, isAlive) {
+    willBeBorn(col, row, cellsAroundAlive) {
+      if (col > 0 && row > 0) {
+        if (this.grid[col - 1][row - 1] === true) {
+          cellsAroundAlive++
+        }
+      }
+      if (row > 0) {
+        if (this.grid[col][row - 1] === true) {
+          cellsAroundAlive++
+        }
+      }
+      if (col < this.grid.length - 1 && row > 0) {
+        if (this.grid[col + 1][row - 1] === true) {
+          cellsAroundAlive++
+        }
+      }
+      if (col > 0) {
+        if (this.grid[col - 1][row] === true) {
+          cellsAroundAlive++
+        }
+      }
+      if (col < this.grid.length - 1) {
+        if (this.grid[col + 1][row] === true) {
+          cellsAroundAlive++
+        }
+      }
+
+      if (row < this.grid[col].length - 1 && col > 0) {
+        if (this.grid[col - 1][row + 1] === true) {
+          cellsAroundAlive++
+        }
+      }
+
+      if (row < this.grid[col].length - 1) {
+        if (this.grid[col][row + 1] === true) {
+          cellsAroundAlive++
+        }
+      }
+      if (row < this.grid[col].length - 1 && col < this.grid.length - 1) {
+        if (this.grid[col + 1][row + 1] === true) {
+          cellsAroundAlive++
+        }
+      }
+
+      // Test si nombre de celulle est suffisant
+      if (cellsAroundAlive === 3) {
+        return true
+      } else {
+        return false
+      }
+    },
+    updateCell(rowIndex, collIndex, isAlive) {
+      //console.log('row index : ' + rowIndex)
+      //console.log('col index : ' + collIndex)
       // Mettre à jour l'état de la cellule dans la grille
-      this.grid[index].isAlive = isAlive;
+      this.grid[rowIndex][collIndex] = isAlive
     },
-    createArray(cols, rows){
-      let arr = new Array(cols);
-      for(let i = 0; i<arr.length; i++){
+    createArray(cols, rows) {
+      let arr = new Array(cols)
+      for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(rows)
       }
       return arr
+    },
+    getCell(rowIndex, cellIndex) {
+      //console.log('getCell : ', this.grid[rowIndex][cellIndex])
+      return this.grid[rowIndex][cellIndex]
     }
   },
   created() {
     // Appeler createArray pour initialiser grid
-    this.grid = this.createArray(this.x, this.y);
-    for(let i = 0; i<this.x; i++){
-      for(let j = 0; j<this.y; j++){
+    this.grid = this.createArray(this.x, this.y)
+    for (let i = 0; i < this.x; i++) {
+      for (let j = 0; j < this.y; j++) {
         this.grid[i][j] = false
       }
     }
@@ -68,7 +137,7 @@ export default {
 </script>
 
 <style scoped>
-.grid{
+.grid {
   display: grid;
 }
 </style>
--- src/components/SimulatorCell.vue
+++ src/components/SimulatorCell.vue
@@ -1,31 +1,35 @@
 <template>
-  <div @click="toggleAlive" class="cell" :class="{alive: isAlive}">
-    
-  </div>
+  <div @click="toggleAlive" class="cell" :class="{ alive: isAlive }"></div>
 </template>
 
 <style scoped>
-.cell{
-  height:50px ;
+.cell {
+  height: 50px;
   border: 1px solid black;
 }
-.alive{
+.alive {
   background-color: rgb(197, 93, 93);
 }
 </style>
 
 <script>
 export default {
-  props: ['initialIsAlive'],
+  props: ['isAliveParent'],
   data() {
     return {
-      isAlive: this.initialIsAlive
-    };
+      isAlive: this.isAliveParent
+    }
   },
-  methods:{
+  watch: {
+    isAliveParent(newValue) {
+      this.isAlive = newValue
+    }
+  },
+  methods: {
     toggleAlive() {
-      this.isAlive = !this.isAlive;
-      this.$emit('update:isAlive', this.isAlive); // Émettre un événement avec la nouvelle valeur de isAlive
+      //console.log(this.isAliveParent)
+      this.isAlive = !this.isAlive
+      this.$emit('update:isAlive', this.isAlive) // Émettre un événement avec la nouvelle valeur de isAlive
     }
   }
 }
