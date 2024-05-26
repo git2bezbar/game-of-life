dd8c814428c15424eedd942f2577e65e772fd93c
Adem <ademduran6888@gmail.com>
2024-05-26 17:10:34 +0200

    feat(app): setting basics views

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,3 @@
-<script>
-import InputWithLabelComponent from './components/InputWithLabel/InputWithLabelComponent.vue'
-import Chip from './components/Chip/ChipComponent.vue'
-import AddingButtonComponent from './components/AddingButton/AddingButtonComponent.vue'
-
-export default {
-  components: {
-    InputWithLabelComponent,
-    Chip,
-    AddingButtonComponent
-  }
-}
-</script>
-
 <template>
-  <div class="h-screen w-screen bg-dark-purple flex flex-col items-center justify-center p-96 gap-16 text-white">
-    <InputWithLabelComponent :label="'My label'" :placeholder="'malafak'" />
-    <Chip>Zifukoro</Chip>
-    <AddingButtonComponent class="w-[350px] h-[150px]"/>
-  </div>
+  <RouterView />
 </template>
-
-<style>
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
-</style>
