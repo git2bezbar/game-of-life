7ce3cb93d44e214ecae534a197f4b00098afde52
Adem <ademduran6888@gmail.com>
2024-05-26 17:09:11 +0200

    feat(components): adding header related components

---
+++ src/components/Header/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <div class="bg-dark-purple px-16 py-8 flex justify-between">
+      <router-link to="/">
+        <img alt="Game of Life logo" class="logo" src="@/assets/game-of-life-logo.svg" width="265" height="50" />
+      </router-link>
+      <SearchInputComponent />
+      <ButtonComponent class="uppercase" :to="'/login'">
+        Connexion
+      </ButtonComponent>
+    </div>
+    <HeaderMenuComponent />
+  </header>
+</template>
+
+<script>
+  import ButtonComponent from '../Button/ButtonComponent.vue'
+  import SearchInputComponent from '../SearchInput/SearchInputComponent.vue'
+  import HeaderMenuComponent from './HeaderMenuComponent.vue'
+
+  export default {
+    name: 'HeaderComponent',
+    components: {
+      ButtonComponent,
+      SearchInputComponent,
+      HeaderMenuComponent,
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header/HeaderMenuItemComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <li class="flex flex-col gap-4">
+    <router-link
+      :class="[
+        isActive(link) ? 'bg-purple ' : 'bg-dark-purple ',
+        'font-bold px-8 py-3 rounded-xl'
+      ]"
+      :to="link"
+    >
+      {{ title }}
+    </router-link>
+    <span
+      :class="[
+        isActive(link) ? 'bg-purple' : 'bg-dark-purple', 
+        'h-[4px] rounded-full'
+      ]"
+    />
+  </li>
+</template>
+
+<script>
+  import { useRoute } from 'vue-router';
+
+  export default {
+    name: 'HeaderMenuItemComponent',
+    setup() {
+      const route = useRoute();
+      const isActive = (path) => route.path === path;
+
+      return { isActive };
+    },
+    props: {
+      link: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  }
+</script>
