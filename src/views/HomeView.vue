5bb5502e3d811619dac20bd439ce3ca3f5344587
Adem <ademduran6888@gmail.com>
2024-05-26 23:23:01 +0200

    feat(components): adding the footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
   import HeaderComponent from '../components/Header/HeaderComponent.vue'
+  import FooterComponent from '../components/Footer/FooterComponent.vue'
 </script>
 
 <template>
   <HeaderComponent />
+  <FooterComponent />
 </template>
