f5d9c4d38974ff4bfe27652c082d2d4285fb4381
Léonard Paillet <leonard.paillet@gmail.com>
2024-05-27 15:38:29 +0200

    create page + architecture page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RulesView from '../views/RulesView.vue'
+import SimulatorView from '../views/SimulatorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,7 +19,7 @@ const router = createRouter({
     {
       path: '/simulator',
       name: 'simulator',
-      component: HomeView
+      component: SimulatorView
     },
     {
       path: '/rules',
---
+++ src/views/SimulatorView.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import HeaderComponent from '../components/Header/HeaderComponent.vue'
+  import FooterComponent from '../components/Footer/FooterComponent.vue'
+</script>
+
+<template>
+    <HeaderComponent />
+    <main class="flex flex-col items-start justify-start px-[512px] py-32 gap-8">
+        <h1 class="text-7xl">Le simulateur</h1>
+        <div>
+            <div>
+                <p>Grille</p>
+                <div>
+                    <input type="number">
+                    <p>X</p>
+                    <input type="number">
+                </div>
+            </div>
+            <div>
+                <p>Génération</p>
+                <p>1</p>
+                <div>
+                    <button>Next</button>
+                    <button>Donner vie</button>
+                    <button>Reset</button>
+                </div>
+            </div>
+        </div>
+    </main>
+    <FooterComponent />
+</template>
