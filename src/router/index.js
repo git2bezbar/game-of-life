8d5e350bfc2708e654e731ce7d6936b9362cfadf
Adem <ademduran6888@gmail.com>
2024-05-29 00:09:35 +0200

    feat(routes): adding route for wiki type pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RulesView from '../views/RulesView.vue'
 import WikiView from '../views/WikiView.vue'
+import WikiTypeView from '../views/WikiTypeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/wiki',
       name: 'wiki',
       component: WikiView
+    },
+    {
+      path: '/wiki/:id',
+      name: 'wikiType',
+      component: WikiTypeView
     },
     {
       path: '/simulator',
