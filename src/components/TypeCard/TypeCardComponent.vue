cef81e1f8ad905af9b904de12bcb17bfb32bd615
Adem <ademduran6888@gmail.com>
2024-05-29 23:21:44 +0200

    feat(components): adding the typecard component

---
+++ src/components/TypeCard/TypeCardComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="bg-purple/10 rounded-lg p-4 flex flex-col items-start gap-2 hover:bg-purple/50 duration-300"
+  >
+    <div class="flex gap-2 justify-end w-full">
+      <img src="@/assets/icons/edit.svg" />
+      <img src="@/assets/icons/delete.svg" />
+    </div>
+    <h4 class="text-2xl">{{ title }}</h4>
+    <p class="text-sm">{{ description.substring(0, 50) + '...' }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TypeCardComponent',    
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+    },
+  }
+</script>
