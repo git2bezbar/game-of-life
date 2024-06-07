4d04f8ff6a4dd608210c8acbf77f5a746596a312
Adem <ademduran6888@gmail.com>
2024-06-07 11:11:45 +0200

    fix(components): adding hover to adding button component

--- src/components/AddingButton/AddingButtonComponent.vue
+++ src/components/AddingButton/AddingButtonComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="p-4 bg-purple/30 rounded-xl flex items-center justify-center">
+  <button class="p-4 bg-purple/30 rounded-xl flex items-center justify-center hover:bg-purple/60 duration-300">
     <img src="@/assets/icons/plus-icon.svg" alt="+">
   </button>
 </template>
