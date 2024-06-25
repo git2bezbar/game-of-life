41470b6d6429450a22dd4105b201c1249a4ba455
Adem <ademduran6888@gmail.com>
2024-06-25 11:56:39 +0200

    fix(components): making the input with label using correct id and for props

--- src/components/InputWithLabel/InputWithLabelComponent.vue
+++ src/components/InputWithLabel/InputWithLabelComponent.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex flex-col items-start gap-4 w-full">
-    <label class="text-lg" :for="label">{{ label }}</label>
+    <label class="text-lg" :for="label.toLowerCase()">{{ label }}</label>
     <input
+      :id="label.toLowerCase()"
       class="p-4 bg-purple/10 border border-purple/40 rounded-2xl outline-none focus:border-purple/80 duration-300 placeholder:text-white/40 min-w-[300px] w-full"
       type="text"
       :value="inputValue"
